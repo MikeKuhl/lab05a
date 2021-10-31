e2092848ce3cb0136a4324face9aa54af1640f1e
MikeKuhl <38599344+MikeKuhl@users.noreply.github.com>
2021-10-31 15:00:16 -0700

    stretch goal complete

--- app.js
+++ app.js
@@ -170,4 +170,4 @@ function multiplyAnyArray(dynamicArray) {
 // Here is the test for multiplyArray(); uncomment it to run it
 // testMultiplyAnyArray(testDynamicArray);
 
-// Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. You're done! Submit the link to the repo following the instructions in Canvas.
+// Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. You're done! Submit the link to the repo following the instructions in Canvas  .
